0eebd0d82afa96cb8cffe9610d2b9d29f2947073
leo15sm <nickname.player15@gmail.com>
2023-12-12 14:34:03 -0300

    commit inicial

---
+++ src/main/java/com/leo/bockstore/BockstoreApplication.java
@@ -0,0 +1,13 @@
+package com.leo.bockstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BockstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BockstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leo/bockstore/BockstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.bockstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BockstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
